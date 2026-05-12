5027f7e2b838168691d7000c48b2d1a82d3240c5
Wilson-Ochieng <wilsonochieng718@gmail.com>
2026-05-12 13:59:12 +0300

    updated homepage

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:smartshop/services/app_manager.dart';
+
+class AppConstants {
+  static const String imageUrl =
+      "https://i.ibb.co/8r1Ny2n/20-Nike-Air-Force-1-07.png";
+
+  static List<String> bannersImage = [
+    AssetsManager.banner1,
+    AssetsManager.banner2,
+    AssetsManager.banner3,
+    
+  ];
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,86 +1,70 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:smartshop/providers/theme_provider.dart';
+import 'package:smartshop/constants/app_constants.dart';
 import 'package:smartshop/services/app_manager.dart';
 import 'package:smartshop/wigets/apptextname.dart';
-import 'package:smartshop/wigets/drawer.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   static const routName = "/HomeScreen";
+
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-  final int _currentIndex = 0; 
-
-  _increment() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _onDrawerItemSelected(int index) {
-    if (index == _currentIndex) {
-      Navigator.pop(context);
-      return;
-    }
-
-    switch (index) {
-      case 0: // Home
-        Navigator.pop(context);
-        break;
-      case 1: // Search
-        Navigator.pop(context);
-        Navigator.pushNamed(context, '/search');
-        break;
-      case 2: // Cart
-        Navigator.pop(context);
-        Navigator.pushNamed(context, '/cart');
-        break;
-      case 3: // Profile
-        Navigator.pop(context);
-        Navigator.pushNamed(context, '/profile');
-        break;
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-        title: const  Apptextname(fontSize: 34.5),
-        backgroundColor: Theme.of(context).primaryColor,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(AssetsManager.shoppingCart),
+        ),
+        title: const Apptextname(fontSize: 20),
       ),
-      drawer: ReusableDrawer(
-        currentIndex: _currentIndex,
-        onItemSelected: _onDrawerItemSelected,
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(AssetsManager.banner1),
-            // ElevatedButton(
-            //   onPressed: _increment, 
-            //   child: const Text("Shop Now")
-            // ),
-            const SizedBox(height: 20),
-            // SwitchListTile(
-            //   title: Text(
-            //     themeProvider.getIsDarkTheme ? "Dark Theme" : "Light theme",
-            //   ),
-            //   value: themeProvider.getIsDarkTheme,
-            //   onChanged: (value) {
-            //     themeProvider.setDarkTheme(value);
-            //   },
-            // ),
-          ],
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 15),
+              SizedBox(
+                height: size.height * 0.25,
+                child: SizedBox(
+                  height: size.height * 0.25,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+
+                    child: Swiper(
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.asset(
+                          AppConstants.bannersImage[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      itemCount: AppConstants.bannersImage.length,
+                      pagination: SwiperPagination(
+                        // alignment: Alignment.center,
+                        builder: DotSwiperPaginationBuilder(
+                          activeColor: Colors.red,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 15.0),
+
+
+             
+            ],
+          ),
         ),
       ),
     );
